b4ff256bb962b7f9df32a8aaf755e09ff5ae3890
NijatRzazade <rzazadenicat009@gmail.com>
2023-11-20 21:17:33 +0400

    lesson-1 task

---
+++ Lesson-1/test.cs
@@ -0,0 +1,14 @@
+using System;
+namespace test{
+    public class Program{
+       public static void Main(){
+
+       Console.WriteLine("Zehmet olmasa adınızı qeyd edin");
+       string name=Console.ReadLine();
+Console.WriteLine("Hello"+name);
+Console.ReadLine();
+}
+
+}
+
+}
