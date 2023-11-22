218611ea885cc9ac2e9e9e4bc876ee9876304f99
NijatRzazade <rzazadenicat009@gmail.com>
2023-11-23 03:14:33 +0400

    Lesson-2

---
+++ Lesson-2/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //+,-,*,/
+            Console.WriteLine("1-ci ededi daxil et");
+            float x1 = (float)Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("2-ci ededi daxil et");
+            float x2 = (float)Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("symbol daxil et");
+            string symbol = Console.ReadLine();
+
+            switch (symbol)
+            {
+                case "+":
+                    Console.WriteLine("Result :" + (x1 + x2));
+                    Console.ReadLine();
+                    break;
+                case "-":
+                    Console.WriteLine("Result :" + (x1 - x2));
+                    Console.ReadLine();
+                    break;
+                case "*":
+                    Console.WriteLine("Result :" + x1 * x2);
+                    Console.ReadLine();
+                    break;
+                case "/":
+                    Console.WriteLine("Result :" + x1 / x2);
+                    Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine("Duzgun simbol daxil edilmeyib");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+}
---
+++ Lesson-2/Lesson-2.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Eli";
+            ////int x = 5;
+            //string[] backend118 = { "Selim", "Nicat.H", "Nicat.R", "Metanet", "Ferid", "Elnure", "Subhan" };
+            //Console.WriteLine(backend118.Length);
+            //Console.WriteLine(backend118[0]);
+            //Console.WriteLine(backend118[backend118.Length-3]);
+            //Console.ReadLine();
+            //for loop
+            //foreach (var x in backend118)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //for(int i=0; i<backend118.Length; i++)
+            //{
+            //    Console.WriteLine(backend118[i]);
+            //    Console.ReadLine();
+            //}
+
+            //for (int i = 1; i < 5; i++)
+            //{
+            //    Console.WriteLine(backend118[i]);
+            //    Console.ReadLine();
+            //}
+
+            //string data = "Nihat";
+            //if (data.Length==5)
+            //{
+            //    Console.WriteLine("5-e beraberdir");
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Berarber deyil");
+            //    Console.ReadLine();            
+            //}
+
+            //int count = 0;
+            //foreach(var i in backend118)
+            //{
+            //    if (i == "Eli")
+            //    {
+            //        count++;
+            //        Console.WriteLine("Bu qrupda Eli adlı telebe var");
+            //        Console.ReadLine();
+            //    }
+            //}
+            //if(count == 0)
+            //{
+            //    Console.WriteLine("Bu qrupda Eli adlı telebe yoxdur");
+            //    Console.ReadLine();
+            //}
+
+            //+,-,*,/
+            //Console.WriteLine("1-ci ededi daxil et");
+            //float x1 = (float)Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("2-ci ededi daxil et");
+            //float x2 = (float)Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("symbol daxil et");
+            //string symbol = Console.ReadLine();
+
+            //if (symbol == "+")
+            //{
+            //    Console.WriteLine("Result :" + x1 + x2);
+            //    Console.ReadLine();
+            //}
+            //else if (symbol == "-")
+            //{
+            //    Console.WriteLine("Result :" + (x1 - x2));
+            //    Console.ReadLine();
+            //}
+            //else if (symbol == "*")
+            //{
+            //    Console.WriteLine("Result :" + x1 * x2);
+            //    Console.ReadLine();
+            //}
+            //else if (symbol == "/")
+            //{
+            //    Console.WriteLine("Result :" + x1 / x2);
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Düzgün symbol daxil edilmeyib");
+            //    Console.ReadLine();
+            //}
+
+            //switch (symbol)
+            //{
+            //    case "+":
+            //        Console.WriteLine("Result :" + (x1 + x2));
+            //        Console.ReadLine();
+            //        break;
+            //    case "-":
+            //        Console.WriteLine("Result :" + (x1 - x2));
+            //        Console.ReadLine();
+            //        break;
+            //    case "*":
+            //        Console.WriteLine("Result :" + x1 * x2);
+            //        Console.ReadLine();
+            //        break;
+            //    case "/":
+            //        Console.WriteLine("Result :" + x1 / x2);
+            //        Console.ReadLine();
+            //        break;
+            //    default:
+            //        Console.WriteLine("Duzgun simbol daxil edilmeyib");
+            //        Console.ReadLine();
+            //        break;
+            //}
+
+            //DayOfWeek tarix = DateTime.Now.DayOfWeek;
+
+            //const double pi = 3.14;
+
+            //switch (tarix.ToString())
+            //{
+            //    case "Tuesday":
+            //        Console.WriteLine("Cersenbe axsami");
+            //        Console.ReadLine();
+            //        break;
+
+            //    case "Monday":
+            //        Console.WriteLine("Bazar ertesi");
+            //        Console.ReadLine();
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Bele heftenin gunu yoxdur");
+            //        Console.ReadLine();
+            //        break;
+            //}
+
+
+        }
+    }
+}
+
