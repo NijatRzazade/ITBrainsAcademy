a14d6fdf4d2226c867e0d137681af8b3be1e5932
NijatRzazade <rzazadenicat009@gmail.com>
2023-11-27 15:46:58 +0400

    Lesson-3

---
+++ Lesson-3/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Metodlar
+            //while (true)
+            //{
+            //    Console.WriteLine("Balını qeyd et");
+            //    int bal=Convert.ToInt32(Console.ReadLine());
+            //    HelloMethod(bal);
+            //}
+
+
+            //HelloMethod(90);
+            //HelloMethod(85);
+            //HelloMethod(71);
+
+
+
+            //while (true)
+            //{
+            //    Console.WriteLine("1-ci ededi daxil et");
+            //    float x1 = (float)Convert.ToDouble(Console.ReadLine());
+            //    Console.WriteLine("2-ci ededi daxil et");
+            //    float x2 = (float)Convert.ToDouble(Console.ReadLine());
+            //    Console.WriteLine("symbol daxil et");
+            //    string symbol = Console.ReadLine();
+
+            //    Calculate(symbol, x1, x2); ;
+            //}
+
+            //int toplaVar = Toplama(20, 15);
+            //Console.WriteLine(toplaVar + 10);
+            #endregion
+            //Fixed size
+            int[] array = { 5, 3, 6, 91, 24, 103, 78, 63 };
+            //1 arrayin en boyuk++++++++
+            //2 en kicik elementi   ++++++++
+            //3 arraydeki datalarin cemi  +++++
+            //4 2e bolunenleri cixar      ++++++++++++
+            //5 ortalama ededi cixar       ++++++++++
+            //6 cut ve tek ededleri cixar  +++++++++++
+            //foreach (int i in array)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //        Console.ReadLine(); 
+            //    }
+            //}
+            //Console.WriteLine(array[0]);
+            //int maxNum = array[0];   //5
+            //int minNum = array[0];   //5
+            //foreach (var item in array)
+            //{
+            //    //if (item > maxNum)   
+            //    //{
+            //    //    maxNum = item;   
+            //    //}
+            //    if (item < minNum)
+            //    {
+            //        minNum = item;
+            //    }
+            //Console.WriteLine(array[0]);
+            //int maxNum = array[0];   //5
+            //int minNum = array[0];   //5
+            //foreach (var item in array)
+            //{
+            //    //if (item > maxNum)   
+            //    //{
+            //    //    maxNum = item;   
+            //    //}
+            //    if (item < minNum)
+            //    {
+            //        minNum = item;
+            //    }
+
+
+            #region Metodlar
+            //public static void Toplama(int data1, int data2)
+            //{
+            //    int result = data1 + data2;
+            //    return result;
+            //}
+
+            //public static void Calculate(string symbol, float x1, float x2)
+            //{
+            //switch (symbol)
+            //{
+            //    case "+":
+            //        Console.WriteLine("Result :" + (x1 + x2));
+            //        Console.ReadLine();
+            //        break;
+            //    case "-":
+            //        Console.WriteLine("Result :" + (x1 - x2));
+            //        Console.ReadLine();
+            //        break;
+            //    case "*":
+            //        Console.WriteLine("Result :" + x1 * x2);
+            //        Console.ReadLine();
+            //        break;
+            //    case "/":
+            //        Console.WriteLine("Result :" + x1 / x2);
+            //        Console.ReadLine();
+            //        break;
+            //    default:
+            //        Console.WriteLine("Duzgun simbol daxil edilmeyib");
+            //        Console.ReadLine();
+            //        break;
+        }
+    }
+
+        //public static void HelloMethod(int grade)
+        //{
+            //switch (grade)
+            //{
+            //    case int g when (g >= 90 && g <= 100):
+            //        Console.WriteLine($"Grade: {grade} - Letter Grade: A");
+            //        Console.ReadLine();
+            //        break;
+            //    case int g when (g >= 80 && g <= 89):
+            //        Console.WriteLine($"Grade: {grade} - Letter Grade: B");
+            //        Console.ReadLine();
+            //        break;
+            //    case int g when (g >= 70 && g <= 79):
+            //        Console.WriteLine($"Grade: {grade} - Letter Grade: C");
+            //        Console.ReadLine();
+            //        break;
+            //    case int g when (g >= 60 && g <= 69):
+            //        Console.WriteLine($"Grade: {grade} - Letter Grade: D");
+            //        Console.ReadLine();
+            //        break;
+            //    default:
+            //        Console.WriteLine($"Grade: {grade} - Letter Grade: F");
+            //        Console.ReadLine();
+            //        break;
+            //}
+        }
+        #endregion
+//}
+
+
+//}
